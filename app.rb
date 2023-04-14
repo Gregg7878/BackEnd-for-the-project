27605ad977510b8fb6be919e96d532126a69d756
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-04-14 19:02:58 +0300

    modified app.rb

--- app.rb
+++ app.rb
@@ -1,47 +1,100 @@
 # app.rb
 
-require 'sinatra/base'
-require 'sinatra/reloader' if development?
-require 'sinatra/activerecord'
-require 'bcrypt'
+require 'sinatra'
+require 'json'
 require_relative 'models/article'
 require_relative 'models/comment'
-require_relative 'models/user'
-require_relative 'routes/article_detail'
-require_relative 'routes/article_form'
-require_relative 'routes/article_list'
-require_relative 'routes/comment_form'
-require_relative 'routes/comment_list'
-require_relative 'routes/login_form'
-require_relative 'routes/user_form'
 
-# Configure database connection
-set :database, { adapter: 'sqlite3', database: 'db/development.sqlite3' }
+# API endpoint to get a list of all articles
+get '/articles' do
+  content_type :json
+  articles = Article.all
+  articles.to_json
+end
 
-class App < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
+# API endpoint to get the details of a specific article
+get '/articles/:id' do
+  content_type :json
+  id = params[:id].to_i
+  article = Article.find(id)
+  if article
+    article.to_json
+  else
+    status 404
   end
+end
 
-  enable :sessions
-
-  # Define helpers for authentication
-  helpers do
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-
-    def authenticated?
-      !current_user.nil?
-    end
-
-    def require_authentication
-      redirect '/login' unless authenticated?
-    end
+# API endpoint to create a new article
+post '/articles' do
+  content_type :json
+  data = JSON.parse(request.body.read)
+  article = Article.new(data)
+  if article.save
+    status 201
+    article.to_json
+  else
+    status 400
+    { error: "Invalid parameters" }.to_json
   end
+end
 
-  # Define root route
-  get '/' do
-    redirect '/articles'
+# API endpoint to update an existing article
+put '/articles/:id' do
+  content_type :json
+  id = params[:id].to_i
+  article = Article.find(id)
+  if article
+    data = JSON.parse(request.body.read)
+    article.update(data)
+    status 200
+    article.to_json
+  else
+    status 404
+  end
+end
+
+# API endpoint to delete an existing article
+delete '/articles/:id' do
+  id = params[:id].to_i
+  article = Article.find(id)
+  if article
+    article.destroy
+    status 204
+  else
+    status 404
+  end
+end
+
+# API endpoint to get a list of comments for a specific article
+get '/articles/:id/comments' do
+  content_type :json
+  id = params[:id].to_i
+  article = Article.find(id)
+  if article
+    comments = article.comments
+    comments.to_json
+  else
+    status 404
+  end
+end
+
+# API endpoint to create a new comment for a specific article
+post '/articles/:id/comments' do
+  content_type :json
+  id = params[:id].to_i
+  article = Article.find(id)
+  if article
+    data = JSON.parse(request.body.read)
+    comment = Comment.new(data)
+    comment.article_id = article.id
+    if comment.save
+      status 201
+      comment.to_json
+    else
+      status 400
+      { error: "Invalid parameters" }.to_json
+    end
+  else
+    status 404
   end
 end
